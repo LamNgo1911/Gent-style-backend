6166b21dc0312a9a40014e425f98b61ac5f9f91b
Diddayngo <lamngo606@gmail.com>
2024-03-18 11:35:19 +0200

    completed - Lam-part

---
+++ src/routers/orderRouter.ts
@@ -0,0 +1,161 @@
+import express, { Request, Response } from "express";
+import { Order, Size } from "../misc/types";
+import { orders } from "../misc/data";
+
+const router = express.Router();
+
+// Lam
+
+// get all orders
+router.get("/", (request: Request, response: Response) => {
+  const userId = request.query.id as string;
+  let newOrders;
+
+  if (userId) {
+    newOrders = orders.filter((order) =>
+      order.id.toLowerCase().includes(userId)
+    );
+  }
+
+  response.status(200).json(newOrders);
+});
+
+// create order
+router.post("/", (request: Request, response: Response) => {
+  const body = request.body;
+  let newOrders = [];
+  //   example
+  const newOrder: Order = {
+    id: "1",
+    quantity: 2,
+    priceSum: 3,
+    products: [
+      {
+        id: "2",
+        name: "order2",
+        price: 2,
+        description: "description2",
+        category: { id: "2", name: "category2" },
+        size: Size.Medium,
+      },
+      {
+        id: "3",
+        name: "order3",
+        price: 3,
+        description: "description3",
+        category: { id: "3", name: "category3" },
+        size: Size.Large,
+      },
+    ],
+  };
+  newOrders = [...orders];
+  newOrders.push(body);
+  response.status(201).json(newOrders);
+});
+
+//  get single order
+router.get("/:orderId", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const index = orders.findIndex((order) => order.id === id);
+    let result = {};
+    if (index !== -1) {
+      result = {
+        success: true,
+        msg: "Order Get Successful",
+        data: orders[index],
+      };
+    } else {
+      result = { success: false, msg: "Order Info not Found", data: [] };
+    }
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(500).send({ success: false, msg: error.message });
+  }
+});
+
+// Lam
+router.put("/:orderId", (request: Request, response: Response) => {
+  const orderId = request.params.orderId;
+  let newOrders;
+  try {
+    const index = orders.findIndex((item) => item.id === orderId);
+    if (index !== -1) {
+      const newOrder: Order = {
+        id: "1",
+        quantity: 2,
+        priceSum: 3,
+        products: [
+          {
+            id: "2",
+            name: "order2",
+            price: 2,
+            description: "description2",
+            category: { id: "2", name: "category2" },
+            size: Size.Medium,
+          },
+          {
+            id: "3",
+            name: "order3",
+            price: 3,
+            description: "description3",
+            category: { id: "3", name: "category3" },
+            size: Size.Large,
+          },
+        ],
+      };
+
+      newOrders = orders.map((order) => {
+        if (Number(order.id) === index) {
+          return {
+            ...order,
+            products: [
+              {
+                id: "4",
+                name: "order4",
+                price: 2,
+                description: "description2",
+                category: { id: "2", name: "category2" },
+                size: Size.Medium,
+              },
+              {
+                id: "5",
+                name: "order5",
+                price: 3,
+                description: "description3",
+                category: { id: "3", name: "category3" },
+                size: Size.Large,
+              },
+            ],
+          };
+        }
+      });
+
+      response.sendStatus(204); // Order updated successfully
+    } else {
+      response.status(404).json({ success: false, msg: "Order not found" });
+    }
+  } catch (error: any) {
+    response.status(500).send({ success: false, msg: error.message });
+  }
+});
+
+// delete order
+router.delete("/:orderId", (request: Request, response: Response) => {
+  const orderId = request.params.orderId;
+
+  try {
+    const index = orders.findIndex((item) => item.id === orderId);
+    if (index !== -1) {
+      orders.splice(index, 1);
+      response.sendStatus(204); // Order deleted successfully
+    } else {
+      response.status(404).json({ success: false, msg: "Order not found" });
+    }
+  } catch (error: any) {
+    response.status(500).send({ success: false, msg: error.message });
+  }
+});
+
+export default router;
--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,13 +1,33 @@
 import express, { Request, Response } from "express";
-import { Size, Category, Product } from "../misc/types"
+import { Size, Category, Product } from "../misc/types";
 
 const router = express.Router();
 
-
 let products: Product[] = [
-  { id: "1", name: "product1", price: 1, description: "description1", category: { id: '1', name: "category1" }, size: Size.Small },
-  { id: "2", name: "product2", price: 2, description: "description2", category: { id: '2', name: "category2" }, size: Size.Medium },
-  { id: "3", name: "product3", price: 3, description: "description3", category: { id: '3', name: "category3" }, size: Size.Large },
+  {
+    id: "1",
+    name: "product1",
+    price: 1,
+    description: "description1",
+    category: { id: "1", name: "category1" },
+    size: Size.Small,
+  },
+  {
+    id: "2",
+    name: "product2",
+    price: 2,
+    description: "description2",
+    category: { id: "2", name: "category2" },
+    size: Size.Medium,
+  },
+  {
+    id: "3",
+    name: "product3",
+    price: 3,
+    description: "description3",
+    category: { id: "3", name: "category3" },
+    size: Size.Large,
+  },
 ];
 
 router.get("/", (request: Request, response: Response) => {
@@ -16,20 +36,18 @@ router.get("/", (request: Request, response: Response) => {
   const categoryQuery = request.query.category as string;
 
   if (nameQuery) {
-    products = products.filter(product =>
+    products = products.filter((product) =>
       product.name.toLowerCase().includes(nameQuery.toLowerCase())
     );
   }
 
   if (priceQuery) {
     const price = parseFloat(priceQuery);
-    products = products.filter(product =>
-      product.price === price
-    );
+    products = products.filter((product) => product.price === price);
   }
 
   if (categoryQuery) {
-    products = products.filter(product =>
+    products = products.filter((product) =>
       product.category.name.includes(categoryQuery)
     );
   }
@@ -37,9 +55,8 @@ router.get("/", (request: Request, response: Response) => {
   response.status(200).json(products);
 });
 
-
 router.post("/", (request: Request, response: Response) => {
-  const body = request.body
+  const body = request.body;
   const newProduct: Product = {
     // Since we don't have DB yet let ID be like this
     id: (products.length + 1).toString(),
@@ -47,11 +64,17 @@ router.post("/", (request: Request, response: Response) => {
     price: body.price,
     description: body.description,
     category: body.category,
-    size: body.size
-  }
+    size: body.size,
+  };
 
-  if (!body.name || !body.price || !body.description || !body.category || !body.size) {
-    return response.status(400).json({ message: "fill out all the fields" })
+  if (
+    !body.name ||
+    !body.price ||
+    !body.description ||
+    !body.category ||
+    !body.size
+  ) {
+    return response.status(400).json({ message: "fill out all the fields" });
   }
 
   products.push(newProduct);
@@ -59,39 +82,70 @@ router.post("/", (request: Request, response: Response) => {
 });
 
 //Noor
-router.get('/:productId', (req: Request, res: Response) => {
+router.get("/:productId", (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
-     const index = products.findIndex((product) => product.id === id);
-     let result = {};
-     if (index !== -1) {
-        
-        result = {'success': true, 'msg': 'Product Get Successful', data: products[index]}
-     } else {
-        result = {'success': false, 'msg': 'Product Info not Found', data: []}
-     }
-     res.status(200).json(result);
+    const index = products.findIndex((product) => product.id === id);
+    let result = {};
+    if (index !== -1) {
+      result = {
+        success: true,
+        msg: "Product Get Successful",
+        data: products[index],
+      };
+    } else {
+      result = { success: false, msg: "Product Info not Found", data: [] };
+    }
+    res.status(200).json(result);
   } catch (error: any) {
-     res.status(500).send({'success': false, 'msg': error.message});
+    res.status(500).send({ success: false, msg: error.message });
   }
-})
+});
 //Noor
+
+// Lam
+router.put("/:productId", (request: Request, response: Response) => {
+  const productId = request.params.productId;
+  try {
+    const index = products.findIndex((item) => item.id === productId);
+    if (index !== -1) {
+      const newProduct: Product = {
+        // Since we don't have DB yet let ID be like this
+        id: products[index].id,
+        name: "Update Product",
+        price: 100,
+        description: "Update Product",
+        category: products[index].category,
+        size: products[index].size,
+      };
+
+      products[index] = newProduct;
+
+      response.sendStatus(204); // Product updated successfully
+    } else {
+      response.status(404).json({ success: false, msg: "Product not found" });
+    }
+  } catch (error: any) {
+    response.status(500).send({ success: false, msg: error.message });
+  }
+});
+// lam
 
 // delete product || muzahid
 router.delete("/:productId", (request: Request, response: Response) => {
   const productId = request.params.productId;
-  try{
+  try {
     const index = products.findIndex((item) => item.id === productId);
     if (index !== -1) {
       products.splice(index, 1);
       response.sendStatus(204); // Product deleted successfully
     } else {
-      response.status(404).json({ success: false, msg:'Product not found' });
+      response.status(404).json({ success: false, msg: "Product not found" });
     }
   } catch (error: any) {
-    response.status(500).send({success: false, msg: error.message});
- }
+    response.status(500).send({ success: false, msg: error.message });
+  }
 });
 
 export default router;
