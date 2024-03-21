f2b123c706719d81fb6f92ad0226754275e90628
Naimul Hassan Noor <naimul.roni@gmail.com>
2024-03-21 08:44:43 +0200

    user error handler added

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,24 +1,48 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import Product from "../models/Product"
 import userService from "../services/user"
 import ProductModel, { ProductDocument } from "../models/Product";
 import User, { UserDocument } from "../models/User";
-export async function getAllUser(_: Request, response: Response) {
-   const users = await userService.getAllUser()
-   if(users.length===0){
-      return response.status(404).json({ message: "Empty User List" });
-   }else{
-      response.status(200).json(users)
+import { BadRequestError, InternalServerError, NotFoundError } from "../errors/ApiError";
+import mongoose from "mongoose";
+
+
+export async function getAllUser(request: Request, response: Response, next: NextFunction) {
+   
+  try {
+      const users = await userService.getAllUser()
+      if(users.length===0){
+         return response.status(404).json({ message: "Empty User List" });
+      }else{
+         response.status(200).json(users)
+      }
+   } catch (error) {
+      next(new InternalServerError("Internal error"))
    }
    
 }
 
-export async function getSingleUser(request: Request, response: Response) {
-   const user = await userService.getSingleUser(request.params.id)
-   if(!user) {
-      return response.status(404).json({ message: "User not found" });
-   }
-      return response.status(201).json(user)
+export async function getSingleUser(request: Request, response: Response, next: NextFunction) {
+  
+   try {
+         const user = await userService.getSingleUser(request.params.id)
+         response.status(201).json(user)
+      } catch (error) {
+         if(error instanceof NotFoundError) {
+            response.status(404).json({
+               message: "User not found",
+            });
+         } else if(error instanceof mongoose.Error.CastError) {
+            response.status(404).json({
+               message: "User not found",
+            });
+            return;
+         }
+   
+         next(new InternalServerError());
+      }
+
+
 }
 
 export async function createUser(request: Request, response: Response) {
@@ -33,19 +57,38 @@ export async function updateUser(request: Request, response: Response) {
    const id = request.params.id;
    const user: Partial<UserDocument> = request.body;
 
-   const updateUser: UserDocument | null = await userService.updateUser(id, user);
-
-   if (!updateUser) {
-      return response.status(404).json({ message: "User not found" });
+    try {
+      const updateUser: UserDocument | null = await userService.updateUser(id, user);
+      response.status(200).json(updateUser);
+   } catch (error) {
+      if (error instanceof BadRequestError) {
+         response.status(400).json({ error: "Invalid request" });
+      } else if (error instanceof NotFoundError) {
+         response.status(404).json({ error: "User not found" });
+      } else if(error instanceof mongoose.Error.CastError) {
+         response.status(404).json({
+            message: "User not found",
+         });
+         return;
+      } else {
+         response.status(500).json({ error: 'Internal Server Error' });
+      }
    }
-
-   response.status(200).json(updateUser);
 }
 
 export async function deleteUser(request: Request, response: Response) {
    const id = request.params.id;
 
-   const data = await userService.deleteUser(id)
-
-   response.status(200).json({  message:" User deleted" }).end()
+   try {
+      const data = await userService.deleteUser(id)
+      response.status(204).json({ message: "User has been deleted" }).end();
+   } catch (error) {
+      if (error instanceof BadRequestError) {
+         response.status(400).json({ error: "Invalid request" });
+      } else if (error instanceof NotFoundError) {
+         response.status(404).json({ error: "User not found" });
+      } else {
+         response.status(500).json({ error: 'Internal Server Error' });
+      }
+   }
 }   
