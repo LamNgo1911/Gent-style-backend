ce94afb939d72dd3d42761bdbf55a953d2bc1990
SahaProdip <prodip.saha2@centria.fi>
2024-04-01 22:56:50 +0300

    jwt logic configure v1

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import passport from "passport";
 
@@ -9,7 +10,9 @@ import orderRouter from "./routers/orderRouter";
 import errorHandler from "./middlewares/errorHandler";
 import checkUserRole from './middlewares/checkUserRole';
 import { googleStrategy, jwtStrategy } from "./config/passport";
+import { authenticateRefreshToken, authenticateToken, authenticateJwtToken } from './middlewares/jwtMiddleware';
 
+const auth = require('./app/controllers/auth');
 
 dotenv.config({ path: ".env" })
 
@@ -28,6 +31,14 @@ app.use('/api/v1/:username/users', (req, res, next) => {
 
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/orders", orderRouter)
+
+// JWT || muzahid
+app.use('/', authenticateToken, require("./routers/api"));
+app.post("/refresh-token", authenticateRefreshToken, auth.refreshToken);
+app.post("/verify-token", authenticateJwtToken, (req: Request, res: Response, next: NextFunction) => {
+	console.log("verify-token", req.ip);
+	return res.json({ status: 200, message: "Verified" });
+});
 
 app.use(errorHandler)
 
---
+++ src/controllers/auth.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+const jwt = require('jsonwebtoken');
+
+exports.refreshToken = async (eq: Request, res: Response, next: NextFunction) => {
+    const payload = {};
+    const options = { expiresIn: process.env.JWT_EXPIRES_IN };
+    const secret = process.env.JWT_SECRET;
+    const token = jwt.sign(payload, secret, options);
+    const refreshOptions = { expiresIn: process.env.REFRESH_TOKEN_LIFE };
+    const refreshSecret = process.env.REFRESH_TOKEN_SECRET;
+    const refreshToken = jwt.sign(payload, refreshSecret, refreshOptions);
+    const output = { token, refreshToken };
+    return res.json(output);
+}
